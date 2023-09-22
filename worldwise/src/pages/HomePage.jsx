0a3e2bc2f6c1252cada56b6ea14755d6f5675980
hassan <hassan.elouardy06@gmail.com>
2023-09-22 09:38:32 +0100

    Add PageNav component

--- worldwise/src/pages/HomePage.jsx
+++ worldwise/src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Home page</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default HomePage;
