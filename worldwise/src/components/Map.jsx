6844d7fc5da5848674df18e73e6a3867adc656f4
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-04 09:57:25 +0100

    Fetching city data in the form

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -23,7 +23,7 @@ function Map() {
     getPosition,
   } = useGeolocation();
 
-const {mapLat,mapLng} = useUrlPosition()
+  const [mapLat,mapLng] = useUrlPosition()
 
   useEffect(
     function () {
