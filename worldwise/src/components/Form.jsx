ab816629198de8999768a29dfe9018fa008afbba
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-05 22:50:04 +0100

    Add cities

--- worldwise/src/components/Form.jsx
+++ worldwise/src/components/Form.jsx
@@ -73,7 +73,6 @@ function Form() {
     navigate('/app/cities')
   }
 
-
   if(isLoadingGeocoding) return <Spinner />
   if(!lat && !lng) return <Message message="Start by clicking somewhere on the map"/>
   if(geocodingError) return <Message message={geocodingError} />
