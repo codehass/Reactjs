cbe8c8bdd4dc60c9206cb9828b7077a64d95a766
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-26 09:00:46 +0100

    closures in effects

--- 13-workout-timer/src/Calculator.js
+++ 13-workout-timer/src/Calculator.js
@@ -37,6 +37,14 @@ function Calculator({ workouts, allowSound }) {
 		[duration, allowSound]
 	);
 
+	useEffect(
+		function () {
+			console.log(duration, sets);
+			document.title = `Your ${number}-exercise workout`;
+		},
+		[number, duration, sets]
+	);
+
 	//const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
 	const mins = Math.floor(duration);
 	const seconds = (duration - mins) * 60;
