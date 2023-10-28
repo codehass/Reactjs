a88ee6158566e766d2c2b749b3c85420ae5f7458
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-29 00:58:18 +0100

    creating the account slice

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from " @reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountsSlice";
 import customerReducer from "./features/customers/customersSlice";
 
